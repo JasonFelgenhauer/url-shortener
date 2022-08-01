3880f3cf5e33fbc3962e2fe555c80f60e54935aa
JasonFelgenhauer <jasonfel1@outlook.be>
2022-08-01 15:06:52 +0200

    Add error page (404, 500)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', uRoutes);
-app.use((req, res, next) => res.status(404).send('404 Not Found'));
-app.use((err, req, res, next) => res.status(500).send('500 Internal Server Error'));
+app.use((req, res, next) => res.status(404).render('404', { title: '404' }));
+app.use((err, req, res, next) => res.status(500).render('500', { title: '500' }));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
