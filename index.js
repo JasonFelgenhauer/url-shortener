2b8f39fec31517652739c4abed5d9ba8721b4789
JasonFelgenhauer <jasonfel1@outlook.be>
2022-12-12 17:26:44 +0100

    Refacto & fix bug

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const userRouter = require('./routes/user');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -31,6 +31,6 @@ app.use(cookieParser());
 
 app.use('/', userRouter);
 app.use((req, res, next) => res.status(404).render('404', { title: '404' }));
-// app.use((err, req, res, next) => res.status(500).render('500', { title: '500' }));
+app.use((err, req, res, next) => res.status(500).render('500', { title: '500' }));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
